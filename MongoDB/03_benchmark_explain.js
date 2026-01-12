92f115c2090dfb4193f4a3b6c291ac3de07c1f1f
xrxr-2 <taixurou67@gmail.com>
2026-01-13 03:44:29 +0800

    Update and rename queries_point_and_range.js to 03_benchmark_explain.js

--- MongoDB/03_benchmark_explain.js
+++ MongoDB/03_benchmark_explain.js
@@ -1,24 +1,24 @@
 use online_retail;
 
-// POINT query (hot key)
+// POINT query (hot key) - with index
 db.transactions
   .find({ StockCode_str: "85123A" })
   .hint({ StockCode_str: 1 })
   .explain("executionStats");
 
-// POINT baseline (force COLLSCAN)
+// POINT query baseline - force COLLSCAN
 db.transactions
   .find({ StockCode_str: "85123A" })
   .hint({ $natural: 1 })
   .explain("executionStats");
 
-// RANGE query
+// RANGE query - with index
 db.transactions
   .find({ StockCode_str: { $gte: "22000", $lte: "23000" } })
   .hint({ StockCode_str: 1 })
   .explain("executionStats");
 
-// RANGE baseline (force COLLSCAN)
+// RANGE query baseline - force COLLSCAN
 db.transactions
   .find({ StockCode_str: { $gte: "22000", $lte: "23000" } })
   .hint({ $natural: 1 })
